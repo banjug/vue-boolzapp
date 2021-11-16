bde60c084c9a05151e5530145294e480ab276529
Samuele <miotto.samuele@gmail.com>
2021-11-16 11:36:05 +0100

    add new message function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+let now = new Date();
+console.log(now);
+
 var app = new Vue({
     el: '#app',
     data: {
@@ -15,93 +18,105 @@ var app = new Vue({
                 visible: true,
                 messages: [
                     {   
-                        date:   '10/01/2020   15:30:55',   
-                        text:   'Hai   portato   a   spasso   il   cane?',   
-                        status:   'sent'   
+                        date: '10/01/2020 15:30:55',   
+                        text: 'Hai portato a spasso il cane?',   
+                        status: 'sent'   
                     },   
                     {   
-                        date:   '10/01/2020   15:50:00',   
-                        text:   'Ricordati   di   dargli   da   mangiare',   
-                        status:   'sent'   
+                        date: '10/01/2020 15:50:00',   
+                        text: 'Ricordati di dargli da mangiare',   
+                        status: 'sent'   
                     },   
                     {   
-                        date:   '10/01/2020   16:15:22',   
-                        text:   'Tutto   fatto!',   
-                        status:   'received'   
+                        date: '10/01/2020 16:15:22',   
+                        text: 'Tutto fatto!',   
+                        status: 'received'   
                     }  
                 ]
             },
             {   
-                userName:   'Fabio',   
-                avatar:   'img/03.svg',   
-                visible:   false,   
-                messages:   [   
+                userName: 'Fabio',   
+                avatar: 'img/03.svg',   
+                visible: false,   
+                messages: [   
                     {   
-                        date:   '20/03/2020   16:30:00',   
-                        text:   'Ciao   come   stai?',   
-                        status:   'sent'   
+                        date: '20/03/2020 16:30:00',   
+                        text: 'Ciao come stai?',   
+                        status: 'sent'   
                     },   
                     {   
-                        date:   '20/03/2020   16:30:55',   
-                        text:   'Bene   grazie!   Stasera   ci   vediamo?',   
-                        status:   'received'   
+                        date: '20/03/2020 16:30:55',   
+                        text: 'Bene grazie! Stasera ci vediamo?',   
+                        status: 'received'   
                     },   
                     {   
-                        date:   '20/03/2020   16:35:00',   
-                        text:   'Mi   piacerebbe   ma   devo   andare   a   fare   la   spesa.',   
-                        status:   'sent'   
+                        date: '20/03/2020 16:35:00',   
+                        text: 'Mi piacerebbe ma devo andare a fare la spesa.',   
+                        status: 'sent'   
                     }   
                 ],   
             },  
             {   
-                userName:   'Samuele',   
-                avatar:   'img/04.svg',   
-                visible:   false,   
-                messages:   [   
+                userName: 'Samuele',   
+                avatar: 'img/04.svg',   
+                visible: false,   
+                messages: [   
                     {   
-                        date:   '28/03/2020   10:10:40',   
-                        text:   'La   Marianna   va   in   campagna',   
-                        status:   'received'   
+                        date: '28/03/2020 10:10:40',   
+                        text: 'La Marianna va in campagna',   
+                        status: 'received'   
                     },   
                     {   
-                        date:   '28/03/2020   10:20:10',   
-                        text:   'Sicuro   di   non   aver   sbagliato   chat?',   
-                        status:   'sent'   
+                        date: '28/03/2020 10:20:10',   
+                        text: 'Sicuro di non aver sbagliato chat?',   
+                        status: 'sent'   
                     },   
                     {   
-                        date:   '28/03/2020   16:15:22',   
-                        text:   'Ah   scusa!',   
-                        status:   'received'    
+                        date: '28/03/2020 16:15:22',   
+                        text: 'Ah scusa!',   
+                        status: 'received'    
                     }   
                 ],   
             },   
             {   
-                userName:   'Luisa',   
-                avatar:   'img/05.svg',   
-                visible:   false,   
-                messages:   [   
+                userName: 'Luisa',   
+                avatar: 'img/05.svg',   
+                visible: false,   
+                messages: [   
                     {   
-                        date:   '10/01/2020   15:30:55',   
-                        text:   'Lo   sai   che   ha   aperto   una   nuova   pizzeria?',   
-                        status:   'sent'   
+                        date: '10/01/2020 15:30:55',   
+                        text: 'Lo sai che ha aperto una nuova pizzeria?',   
+                        status: 'sent'   
                     },   
                     {   
-                        date:   '10/01/2020   15:50:00',   
-                        text:   'Si,   ma   preferirei   andare   al   cinema',   
-                        status:   'received'   
+                        date: '10/01/2020 15:50:00',   
+                        text: 'Si, ma preferirei andare al cinema',   
+                        status: 'received'   
                     } 
                 ],   
             }, 
-        ]
+        ],
+        newMessage: {
+            date: now,
+            text: '',
+            status: 'sent'
+        }
     },
-    // al click cambia valore di visible (utilizzato per cambiare classe active al selettore dei contatti e stampare i relativi messaggi)
     methods: {
-        chatOpen(index) {
+        // al click cambia valore di visible (utilizzato per cambiare classe active al selettore dei contatti e stampare i relativi messaggi)
+        chatOpen(index){
             if (this.contacts[index].visible === false) {
                 for (let i = 0; i < this.contacts.length; i++) {
                     this.contacts[i].visible = false;
                 }
                 this.contacts[index].visible = true;
+            }
+        },
+        // invia un nuovo messaggio salvando il valore dell'input
+        sendMessage(index){
+            if (this.newMessage.text[0] !== ' ' && this.newMessage.text.length > 0) {
+                this.contacts[index].messages.push(this.newMessage);
+                this.newMessage = {date: now, text: '', status: 'sent'};
             }
         }
 
